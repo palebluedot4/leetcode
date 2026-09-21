262ca9771c71b8290080a15588cd3cdc10f4c0df
Bruce <162610581+palebluedot4@users.noreply.github.com>
2026-09-21 13:56:34 +0800

    feat(typescript): add recursive variant to 0021 merge two sorted lists

--- problems/0021-merge-two-sorted-lists/solution.ts
+++ problems/0021-merge-two-sorted-lists/solution.ts
@@ -24,3 +24,21 @@ function mergeTwoLists(
   tail.next = list1 ?? list2;
   return dummy.next;
 }
+
+function mergeTwoListsRecursive(
+  list1: ListNode | null,
+  list2: ListNode | null,
+): ListNode | null {
+  if (list1 === null) {
+    return list2;
+  }
+  if (list2 === null) {
+    return list1;
+  }
+  if (list1.val <= list2.val) {
+    list1.next = mergeTwoListsRecursive(list1.next, list2);
+    return list1;
+  }
+  list2.next = mergeTwoListsRecursive(list1, list2.next);
+  return list2;
+}
